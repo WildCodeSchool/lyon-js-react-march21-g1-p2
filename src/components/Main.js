2e6444c0f2faae0a42650934172cfed2f0d36cf6
Cécile Dubois <duboiscecilepro@gmail.com>
2021-04-08 15:57:00 +0200

    changed pre commit of Husky

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,14 +3,18 @@ import HomePage from '../screens/HomePage';
 import ContactAboutPage from '../screens/ContactAboutPage';
 import PredefinedFoodPage from '../screens/PredefinedFoodPage';
 import CustomizedFoodPage from '../screens/CustomizedFoodPage';
+import FavoriteFoodPage from '../screens/FavoriteFoodPage';
+import SignInPage from '../screens/SignInPage';
 
 export default function Main() {
   return (
     <main>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/menu" component={PredefinedFoodPage} />
-        <Route exact path="/custo" component={CustomizedFoodPage} />
+        <Route exact path="/frommenu" component={PredefinedFoodPage} />
+        <Route exact path="/pizzaperso" component={CustomizedFoodPage} />
+        <Route exact path="/favpizza" component={FavoriteFoodPage} />
+        <Route exact path="/login" component={SignInPage} />
         <Route exact path="/contact" component={ContactAboutPage} />
       </Switch>
     </main>
