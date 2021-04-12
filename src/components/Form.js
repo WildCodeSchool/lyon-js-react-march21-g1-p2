ecf7369b4b9e6b5da1f3aabd56c6e6218bfe5244
BuG-Dev-Joris <jorismaupied@gmail.com>
2021-04-12 10:12:09 +0200

    contact form half working

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,47 @@
 export default function Form() {
+  const firstName = '';
+  const lastName = '';
+  const handleFirstNameChange = '';
+  const handleLastNameChange = '';
+  const handleUrTextChange = '';
+  const urText = '';
   return (
     <div>
       <div>
-        <label htmlFor="fname">First name</label>
-        <input type="text" id="fname" name="fname" />
+        <label htmlFor="firstName">
+          Your first name :
+          <input
+            id="firstName"
+            type="text"
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
+        </label>
       </div>
       <div>
-        <label htmlFor="lname">Last name</label>
-        <input type="text" id="lname" name="lname" />
+        <label htmlFor="lastName">
+          Your last name :
+          <input
+            id="lastName"
+            type="text"
+            value={lastName}
+            onChange={handleLastNameChange}
+          />
+        </label>
       </div>
       <div>
-        <label htmlFor="urtext">Your message</label>
-        <textarea id="urtext" name="urtext" placeholder="Give us your point" />
-        <input type="submit" value="Submit" />
+        <label htmlFor="urText">
+          Your message :
+          <input
+            id="urText"
+            type="text"
+            value={urText}
+            onChange={handleUrTextChange}
+          />
+        </label>
+        <div>
+          <input id="submit" type="submit" value="Submit" />
+        </div>
       </div>
     </div>
   );
