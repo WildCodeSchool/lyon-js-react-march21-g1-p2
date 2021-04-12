e4fba85885cf6181e0182bd4f76a4f627fe91953
BuG-Dev-Joris <jorismaupied@gmail.com>
2021-04-12 17:52:40 +0200

    adding emptypizza component to orderpage

---
+++ src/components/EmptyPizza.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import emptypizza from './emptyPizza.png';
+
+export default function EmptyPizzapizza() {
+  return (
+    <div>
+      return <img src={emptypizza} alt="Logo" />
+    </div>
+  );
+}
--- src/screens/CustomizedFoodPage.js
+++ src/screens/CustomizedFoodPage.js
@@ -1,7 +1,10 @@
+import EmptyPizza from '../components/EmptyPizza';
+
 export default function CustomizedFoodPage() {
   return (
     <>
       <p>CustomizedFoodPage</p>
+      <EmptyPizza />
     </>
   );
 }
