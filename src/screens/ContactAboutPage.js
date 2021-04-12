2b8faadea8c2f3fccba39f4a060dd9876a412874
BuG-Dev-Joris <jorismaupied@gmail.com>
2021-04-12 18:04:28 +0200

    deleted second header on contact page

--- src/screens/ContactAboutPage.js
+++ src/screens/ContactAboutPage.js
@@ -1,10 +1,8 @@
-import Header from '../components/Header';
 import Form from '../components/Form';
 
 export default function ContactAboutPage() {
   return (
     <div>
-      <Header />
       <p>ContactAboutPage</p>
 
       <Form />
