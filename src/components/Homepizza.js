2d5ca0cdb0d65f234bebd5ba51ae6953e70f3023
BuG-Dev-Joris <jorismaupied@gmail.com>
2021-04-12 17:41:37 +0200

    adding an awesome pizza image on homepage

---
+++ src/components/Homepizza.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import pizza from './pizza.png';
+
+export default function Homepizza() {
+  return (
+    <div>
+      return <img src={pizza} alt="Logo" />
+    </div>
+  );
+}
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,7 +1,10 @@
+import Homepizza from '../components/Homepizza';
+
 export default function HomePage() {
   return (
     <>
       <p>HomePage</p>
+      <Homepizza />
     </>
   );
 }
