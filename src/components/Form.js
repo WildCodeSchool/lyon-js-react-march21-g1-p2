5e14d13e88a2e9c65331a63476de5929d76efe6c
BuG-Dev-Joris <jorismaupied@gmail.com>
2021-04-14 09:01:54 +0200

    try of dynamic blog with form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,48 +1,84 @@
+// import { useState } from 'react';
+
 export default function Form() {
-  const firstName = '';
-  const lastName = '';
-  const handleFirstNameChange = '';
-  const handleLastNameChange = '';
-  const handleUrTextChange = '';
-  const urText = '';
+  const userId = '';
+  const userEmail = '';
+  const handleAddUserId = '';
+  const handleUserEmailChange = '';
+  const handleAddUserMessage = '';
+  const userMessage = '';
+
+  // const messagesDiplayed = [{ userId, userMessage }];
+  // const [userMessagesList, setUserMessagesList] = useState(messagesDiplayed);
+  // const [userIdList, setUserIdList] = useState(messagesDiplayed);
+
   return (
     <div>
-      <div>
-        <label htmlFor="firstName">
-          Your first name :
-          <input
-            id="firstName"
-            type="text"
-            value={firstName}
-            onChange={handleFirstNameChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="lastName">
-          Your last name :
-          <input
-            id="lastName"
-            type="text"
-            value={lastName}
-            onChange={handleLastNameChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="urText">
-          Your message :
-          <input
-            id="urText"
-            type="text"
-            value={urText}
-            onChange={handleUrTextChange}
-          />
-        </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Pseudo</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>userI d</td>
+            <td>user Message</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <form>
+        <h2>Contact-us !</h2>
         <div>
-          <input id="submit" type="submit" value="Submit" />
+          <div>
+            <label htmlFor="firstName">
+              ID :
+              <input
+                id="userId"
+                type="text"
+                required
+                value={userId}
+                onChange={(event) =>
+                  handleAddUserId(userId.id, event.target.value)
+                }
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="userEmail">
+              email :
+              <input
+                id="userEmail"
+                type="text"
+                required
+                value={userEmail}
+                onChange={handleUserEmailChange}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="userMessage">
+              message :
+              <input
+                id="userMessage"
+                type="text"
+                required
+                value={userMessage}
+                onChange={(event) =>
+                  handleAddUserMessage(userId.message, event.target.value)
+                }
+              />
+            </label>
+            <div>
+              <button id="submit" type="submit" value="Submit">
+                Send
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
