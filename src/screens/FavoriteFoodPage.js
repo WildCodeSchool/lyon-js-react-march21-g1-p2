8cab1f59690adff9ef5da9e195dfad01cad4be39
Cécile Dubois <duboiscecilepro@gmail.com>
2021-04-19 17:40:25 +0200

    Corrected the problems

--- src/screens/FavoriteFoodPage.js
+++ src/screens/FavoriteFoodPage.js
@@ -1,22 +1,7 @@
-import { NavLink, useLocation } from 'react-router-dom';
-import CustomizedFoodPage from './CustomizedFoodPage';
-
 export default function FavoriteFoodPage() {
-  const location = useLocation();
-  const goToPreviousPath = location.pathname;
-  console.log(goToPreviousPath);
-
-  const selectedIngredients = [''];
   return (
     <>
-      <p>FavoriteFoodPage</p>
-      <button type="button" onClick={goToPreviousPath}>
-        <NavLink to="/pizzaperso">Go to custom</NavLink>
-      </button>
-      <CustomizedFoodPage
-        selectedIngredients={selectedIngredients}
-        previouspath={goToPreviousPath}
-      />
+      <p>FavoritePage</p>
     </>
   );
 }
