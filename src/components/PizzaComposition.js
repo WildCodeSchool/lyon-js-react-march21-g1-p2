1ce40a0331cba1e51bb3867753183b1d20d4c429
Solene <solene.gerard@gmail.com>
2021-04-21 19:22:49 +0200

    added background color and border radius to pizza cards

--- src/components/PizzaComposition.js
+++ src/components/PizzaComposition.js
@@ -4,7 +4,7 @@ import pizzabox from '../assets/pizzabox.png';
 const PizzaComposition = ({ name, ingredients, nutrition, image, price }) => (
   <div className="Cards">
     <div className="Card">
-      <figure className="PizzaList flex flex-row mt-10 max-w-min m-auto">
+      <figure className="PizzaList flex flex-row mt-10 max-w-min m-auto bg-yellow-100 ">
         <img src={image} alt={name} className="max-w-xs m-auto" />
         <figcaption className="Figcaption flex flex-col justify-center w-50">
           <div className="m-6 text-yellow-900 font-bold text-2xl text-center">
