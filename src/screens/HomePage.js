b681c2813401bdfbe08eb80dce6e6cc5906cb9fb
Nels-J <jesus.nels@gmail.com>
2021-04-23 14:08:35 +0200

    minor changes chalk dependencies uninstalled

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -3,24 +3,23 @@ import Homepizza from '../components/Homepizza';
 
 export default function HomePage() {
   return (
-
     <div>
       <div>
         <h1 className="m-6 font-bold text-lg text-center">
           Serez-vous notre pizzaïolo du mois ?
         </h1>
-        <p>Chez U’Pizz, nous vous proposons depuis notre fidèle Food Truck toutes
-        les saveurs de l’Italie dans nos pizzas, et vous pourrez mettre la main
-        à la pâte en ajoutant les ingrédients de votre choix de la liste de
-        produits frais mis à votre disposition. <br />
-        Notre emplacement à Confluence vous offre un magnifique écrin de verdure
-        au cœur de la ville, de jour comme de nuit. <br />
-    </p>
+        <p>
+          Chez U’Pizz, nous vous proposons depuis notre fidèle Food Truck toutes
+          les saveurs de l’Italie dans nos pizzas, et vous pourrez mettre la
+          main à la pâte en ajoutant les ingrédients de votre choix de la liste
+          de produits frais mis à votre disposition. <br />
+          Notre emplacement à Confluence vous offre un magnifique écrin de
+          verdure au cœur de la ville, de jour comme de nuit. <br />
+        </p>
         <h2 className="font-bold text-lg text-left">
           Prenez place sur notre terrasse au bord du fleuve ! <br />A vos
-          papilles
+          papilles !
         </h2>
-        !
       </div>
       <div className="flex flex-col justify-center items-center ">
         <NavLink to="/pizzaperso">
@@ -40,7 +39,6 @@ export default function HomePage() {
           <Homepizza />
         </NavLink>
       </div>
-
     </div>
   );
 }
