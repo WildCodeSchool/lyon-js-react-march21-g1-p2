c50a28c57bfa5471d82b054b9830e6bb0294b5e7
Solene <solene.gerard@gmail.com>
2021-04-28 00:54:35 +0200

    gave attributions for API and designs in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,13 @@
 export default function Footer() {
-  return <footer className="footer">footer</footer>;
+  return (
+    <footer className="footer inline-grid gap-2 grid-cols-2 ">
+      <div className="badge" id="edamam-badge" data-color="transparent" />
+      <div className="text-xs text-right">
+        <a id="attribution" href="https://all-free-download.com/">
+          🍕 design provided by All Free Download
+        </a>
+        <p>Ham and pineapple designed by Freepik</p>
+      </div>
+    </footer>
+  );
 }
