026eca6c4910f0dd3a39105c320aed32bff5f633
Solene <solene.gerard@gmail.com>
2021-04-29 12:32:26 +0200

    Revert "merged orderpage into footer"
    This reverts commit 64ba9f27517bcdb19b34dd142c2cd1ca97b92c83, reversing
    changes made to 97e58bde5761dfc60be3f4c704aa6e0bfb75b7d7.

--- src/screens/OrderPage.js
+++ src/screens/OrderPage.js
@@ -1,8 +1,5 @@
-import { Link } from 'react-router-dom';
+import { BtnLink } from '../components/Navbar/NavbarElements';
 import './order.css';
-import pizzaImgFav from '../assets/margarita.png';
-import pizzaImgCreate from '../assets/pizzabox-btn.svg';
-import pizzaImg from '../assets/regina.png';
 
 const favoritePizza = 'Vos Pizzas Favorites';
 const createPizza = 'Créez votre pizza';
@@ -10,39 +7,22 @@ const ourPizza = 'Nos Pizzas';
 
 export default function OrderPage() {
   return (
-    <section className="orderContainer">
-      <ul className="orderGrid">
-        <li className="orderGridCase">
-          <Link className="orderImage" to="/order/pizza-list">
-            <img
-              className="orderImage"
-              src={pizzaImg}
-              alt="Vos Pizzas Favorites"
-            />
-            <h3 className="h3">{ourPizza}</h3>
-          </Link>
-        </li>
-        <li className="orderGridCase">
-          <Link className="orderImage" to="/order/fav-pizza">
-            <img
-              className="orderImage"
-              src={pizzaImgFav}
-              alt="Vos Pizzas Favorites"
-            />
-            <h3 className="h3">{favoritePizza}</h3>
-          </Link>
-        </li>
-        <li className="orderGridCase">
-          <Link className="orderImage" to="/order/create-pizza">
-            <img
-              className="orderImage"
-              src={pizzaImgCreate}
-              alt="Vos Pizzas Favorites"
-            />
-            <h3 className="h3">{createPizza}</h3>
-          </Link>
-        </li>
-      </ul>
-    </section>
+    <div
+      style={{
+        height: '90vh',
+      }}
+    >
+      <div className="stylingInFlex">
+        <BtnLink className="stylingOrderButton" to="/order/fav-pizza">
+          {favoritePizza}
+        </BtnLink>
+        <BtnLink className="stylingOrderButton" to="/order/create-pizza">
+          {createPizza}
+        </BtnLink>
+        <BtnLink className="stylingOrderButton" to="/order/pizza-list">
+          {ourPizza}
+        </BtnLink>
+      </div>
+    </div>
   );
 }
