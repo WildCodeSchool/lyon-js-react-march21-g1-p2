4859db1cac73a8b15eff8f1f60baba8874f7ccb2
BuG-Dev-Joris <maupied69@hotmail.com>
2021-05-02 18:30:35 +0200

    try of lin kapi to email

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,8 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 // import API from '../APIClient';
 
 // email
-
-const emailer = require('./emailer.js');
 
 // livre d'or
 export default function Form() {
@@ -12,32 +10,35 @@ export default function Form() {
   const [userInput, setUserInput] = React.useState('');
   const [userPseudo, setUserPseudo] = React.useState('');
 
-  const [emailFname, setEmailFname] = React.useState('');
-  const [emailLname, setEmailLname] = React.useState('');
-  const [emailEmail, setEmailEmail] = React.useState('');
-  const [emailMessage, setEmailMessage] = React.useState('');
+  const [emailInputs, setEmailInputs] = React.useState({
+    email: '',
+    name: '',
+    subject: '',
+    description: '',
+  });
 
-  const handleSubmit = (event) => {
+  const handleBookSubmit = (event) => {
     event.preventDefault();
     setUserInput('');
     setUserPseudo('');
     setMessages([...messages, `${userPseudo}: ${userInput}`]);
   };
-  const handleEmailSubmit = (event) => {
-    event.preventDefault();
-    emailer.sendMail(
-      {
-        from: 'maupied69@hotmail.com',
-        to: 'maupied69@hotmail.com',
-        subject: "Vous avez recu un message de la part d'un utilisateur",
-        text: `${emailFname}${emailLname}\n${emailMessage}`,
-        html: `<p>${emailFname}${emailLname}\n${emailMessage}</p>`,
-      },
-      (err, info) => {
-        if (err) console.error(err);
-        else console.log(info);
-      }
-    );
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEmailInputs((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // destructure from inputs
+    const { email, name, subject, description } = emailInputs;
+    axios.post('/sendtome', {
+      // make an object to be handled from req.body on the backend.
+      email,
+      name,
+      subject,
+      // change the name to represent text on the backend.
+      text: description,
+    });
   };
   return (
     <div>
@@ -57,7 +58,7 @@ export default function Form() {
       <div className="container">
         <form
           className="bg-blue text-center max-w-lg px-3 py-4 text-black mx-auto rounded"
-          onSubmit={handleSubmit}
+          onSubmit={handleBookSubmit}
         >
           <h2 className="bg-blue text-center max-w-lg px-3 py-4 text-black mx-auto rounded">
             laissez nous vos messages
@@ -88,95 +89,43 @@ export default function Form() {
           </button>
         </form>
       </div>
-
       <>
-        <form className="w-full max-w-lg mx-auto" onSubmit={handleEmailSubmit}>
-          <div className="flex flex-wrap -mx-3 mb-6 ">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-first-name"
-              >
-                First Name
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="Jane"
-                  onChange={(event) => setEmailFname(event.target.value)}
-                  value={emailFname}
-                />
-              </label>
-
-              <p className="text-red-500 text-xs italic">
-                Please fill out this field.
-              </p>
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-last-name"
-              >
-                Last Name
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-2xl py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-last-name"
-                  type="text"
-                  placeholder="Doe"
-                  onChange={(event) => setEmailLname(event.target.value)}
-                  value={emailLname}
-                />
-              </label>
-            </div>
-          </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-password"
-              >
-                E-mail
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="email"
-                  type="email"
-                  onChange={(event) => setEmailEmail(event.target.value)}
-                  value={emailEmail}
-                />
-              </label>
-
-              <p className="text-gray-600 text-xs italic">
-                Some tips - as long as needed
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-password"
-              >
-                Message
-                <textarea
-                  className=" resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-                  id="message"
-                  onChange={(event) => setEmailMessage(event.target.value)}
-                  value={emailMessage}
-                />
-              </label>
-            </div>
-          </div>
-          <div className="md:flex md:items-center">
-            <div className="md:w-1/3">
-              <button
-                className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                type="submit"
-              >
-                Send
-              </button>
-            </div>
-            <div className="md:w-2/3" />
-          </div>
+        <h1>feed back form. </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="email"
+            name="email"
+            value={emailInputs.email}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="name"
+            name="name"
+            value={emailInputs.name}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="subject"
+            name="subject"
+            value={emailInputs.subject}
+            onChange={handleChange}
+          />
+          <br />
+          <textarea
+            name="description"
+            placeholder="tell us about your experience"
+            value={emailInputs.description}
+            onChange={handleChange}
+            cols="30"
+            rows="10"
+          />
+          <br />
+          <button type="submit">submit</button>
         </form>
       </>
     </div>
