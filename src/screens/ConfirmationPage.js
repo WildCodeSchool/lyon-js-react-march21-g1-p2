0f6fb52546cb6294e845368387489f58379740c7
Cécile Dubois <duboiscecilepro@gmail.com>
2021-05-05 12:28:44 +0200

    Corrected a bug on ConfirmationPage (show the data and execute the Post request only if dataForConfirmation is not empty

--- src/screens/ConfirmationPage.js
+++ src/screens/ConfirmationPage.js
@@ -5,33 +5,23 @@ import API from '../APIClient';
 export default function ConfirmationPage() {
   const [error, setError] = useState('');
   const [success, setSuccess] = useState(false);
+  const [showData, setShowData] = useState(null);
   const location = useLocation();
   const dataForConfirmation =
-    location.state != null ? location.state.dataForConfirmation : [];
+    location.state != null ? location.state.dataForConfirmation : null;
 
   // Adding this order data to the database
   useEffect(() => {
-    API.post('/orders', dataForConfirmation)
-      .then(() => {
-        setSuccess(true);
-      })
-      .catch(() => {
-        setError('Cannot record this order.');
-      });
-  }, []);
+    if (dataForConfirmation) {
+      API.post('/orders', dataForConfirmation)
+        .then(() => {
+          setSuccess(true);
+        })
+        .catch(() => {
+          setError('Cannot record this order.');
+        });
 
-  return (
-    <>
-      <h2 className="text-3xl text-center font-bold m-3">
-        Récapitulatif de votre commande
-      </h2>
-      <table id="orders-recap">
-        <thead>
-          <tr>
-            <th>Ingrédients</th>
-            <th>Prix total</th>
-          </tr>
-        </thead>
+      setShowData(
         <tbody>
           <tr>
             <td>
@@ -46,6 +36,23 @@ export default function ConfirmationPage() {
             <td>{dataForConfirmation.price} €</td>
           </tr>
         </tbody>
+      );
+    }
+  }, []);
+
+  return (
+    <>
+      <h2 className="text-3xl text-center font-bold m-3">
+        Récapitulatif de votre commande
+      </h2>
+      <table id="orders-recap">
+        <thead>
+          <tr>
+            <th>Ingrédients</th>
+            <th>Prix total</th>
+          </tr>
+        </thead>
+        {showData}
       </table>
 
       {error && <h3 className="text-2xl font-bold m-3">{error}</h3>}
